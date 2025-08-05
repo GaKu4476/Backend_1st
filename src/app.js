94d164efa2ad5b10d015a1d41bcdd76381d27eff
Gaurav Kumar <0304gaurav@gmail.com>
2025-08-05 08:51:57 +0530

    cors origin change

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ dotenv.config();
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+  origin: '*'
+}));
+
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
